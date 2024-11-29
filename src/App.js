ce5c5f1b431f0dbf6ccfd92d37a6cba483d2a2d7
Tartakovskyi Artem <artem.tartakovskiy@gmail.com>
2024-11-29 01:08:43 +0100

    Create a Search component with React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BsFillCalendar2CheckFill } from "react-icons/bs";
+import { Col, Container, Row } from "react-bootstrap";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className="text-center fw-light mt-3">
+              <BsFillCalendar2CheckFill /> Appointments
+            </h1>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col md={4}>
+            <Search />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+          <Col md={3}>
+            <h3>Hello Bootstrap!</h3>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,33 @@
+import {
+  Dropdown,
+  DropdownButton,
+  FormControl,
+  InputGroup,
+} from "react-bootstrap";
+
+const DropDown = () => {
+  return (
+    <>
+      <DropdownButton variant="outline-secondary" title="Dropdown">
+        <Dropdown.Item href="#">Action</Dropdown.Item>
+        <Dropdown.Item href="#">Another action</Dropdown.Item>
+        <Dropdown.Item href="#">Something else here</Dropdown.Item>
+        <Dropdown.Divider />
+        <Dropdown.Item href="#">Separated link</Dropdown.Item>
+      </DropdownButton>
+    </>
+  );
+};
+
+const Search = () => {
+  return (
+    <>
+      <InputGroup className="mb-3">
+        <FormControl placeholder="Search" />
+        <DropDown />
+      </InputGroup>
+    </>
+  );
+};
+
+export default Search;
